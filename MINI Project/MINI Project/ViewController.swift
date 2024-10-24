056663dc74a773b68bb5bcdaaaa73737d85bc834
ydam19 <dam0109@icloud.com>
2024-10-24 19:07:00 +0900

    Add files via upload

---
+++ MINI Project/MINI Project/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MINI Project
+//
+//  Created by 김예담의 Mac on 10/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func TeamBTN(_ sender: Any) {
+        
+    }
+    
+}
+
